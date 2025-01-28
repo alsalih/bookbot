98b8afdcbc677924d19f912a1d74a891260786e0
haidar-alsalih <haidaralsalih13@gmail.com>
2025-01-28 19:36:10 +0400

    improve word frequencies function and refactor codebase

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ def main():
        sentence, and paragraph counts and mean lengths, most and least frequent 
        words, as well as alphabet frequencies."""
     # ************************************************************************#
-    # section 1: user chooses text for analyser to analyse
+    # Section 1: user chooses text for analyser to analyse
     # ************************************************************************#
 
-    # library is set to the directory/folder where texts are stored
+    # Library is set to the directory/folder where texts are stored
     library = 'texts'
     list_texts(library)
 
-    # get path to user's desired text
+    # Loop to get path to user's desired text
     while True:
         text_title = input("\nEnter name of text to analyse: ")
         text_path = f"{library}/" + "-".join(text_title.split(" ")).lower() + ".txt"
@@ -38,11 +38,13 @@ def main():
             print(f"Error: The file {text_path} was not found. Please try again.")
 
     # ************************************************************************#
-    # section 2: analyser analyses the text and displays the results in a report
+    # Section 2: analyser analyses the text and displays the results in a report
     # ************************************************************************#
 
     text_matter = get_text_matter(text_path)
     report_dict = {}
+
+    # Basic statistics
 
     words = get_words(text_matter)
     report_dict["num_words"] = len(words)
@@ -55,13 +57,14 @@ def main():
     paragraphs = get_paragraphs(text_matter)
     report_dict["num_paragraphs"] = len(paragraphs)
     report_dict["parags_mean_len"] = round(mean_len(paragraphs, "words"), 2)
+
+    # Word frequencies
+
     report_dict["word_frequencies"] = word_frequencies(words)
 
-    print(get_text_report(text_title, report_dict))
-    foo = []
-    for word in report_dict["word_frequencies"][-1::-1]:
-        if word[1] > len(words) / 500:
-            foo.append(word)
+    text_report = get_text_report(text_title, report_dict)
+
+    print(text_report)
 
 # *** END main() *** 
 
@@ -130,11 +133,31 @@ def get_paragraphs(text_matter):
 def word_frequencies(words):
     freq_dict = {}
     for word in words:
-        if word in freq_dict:
-            freq_dict[word] += 1
+        if len(word) == 1:
+            cp_word = word.strip(".,!?\"")
         else:
-            freq_dict[word] = 1
+            cp_word = word.strip(".,!?\"").lower()
+
+        if cp_word in freq_dict:
+            freq_dict[cp_word] += 1
+        else:
+            freq_dict[cp_word] = 1
+        
     return list(sorted(freq_dict.items(), key=lambda x: x[1], reverse=True))
+
+def get_most_frequent_words(word_frequencies):
+    """given a list of word frequencies, returns the 10 most frequent words."""
+    return word_frequencies[:10]
+
+def get_least_frequent_words(word_frequencies):
+    """given a list of word frequencies, returns the 10 least frequent words,
+        with the word occuring at least 0.2% of the time."""
+    
+    least_freq_words = []
+    for word in word_frequencies:
+        if word[1] > len(word_frequencies) / 500:
+            least_freq_words.append(word)
+    return least_freq_words[-11:-1]
 
 def get_text_report(text_title, report_dict):
     """
@@ -144,9 +167,11 @@ def get_text_report(text_title, report_dict):
     text_title (str): The title of the text being analyzed.
     report_dict (dict): A dictionary containing the analysis results.
 
-    Returns a formatted string containing the text report.
+    Returns:
+    a formatted string containing the text report.
     """
     
+    # various statistics
     num_words = report_dict["num_words"]
     words_mean_length = report_dict["words_mean_length"]
     num_sentences = report_dict["num_sentences"]
@@ -154,51 +179,53 @@ def get_text_report(text_title, report_dict):
     num_paragraphs = report_dict["num_paragraphs"]
     parags_mean_len = report_dict["parags_mean_len"]
 
-    
-    
-    def space(n, content):
-        x = int((n - len(str(content))) / 2)
-        return " " * x + str(content) + " " * x
-    
-    stats = lambda x, y: f"|{space(22, x)}|{space(26, y)}|\n"
+    # 10 most and 10 least frequent words
+    most_frequent_words_lst = get_most_frequent_words(report_dict["word_frequencies"])
+    least_frequent_words_lst = get_least_frequent_words(report_dict["word_frequencies"])
 
-    cell_border = "-" * 50 + "\n"
+    stats = lambda x, y: f"|{x:^25}|{y:^25}|\n"
 
-    text_report = "\n...\n"
-    text_report += "\nThank you for using Text Analyser!\n\n"
-    text_report += f"Here is your text report on {text_title.title()}:\n\n"
+    cell_border = "-" * 53 + "\n"
+
+    # Greeting
+    text_report = "\n...\n\nThank you for using Text Analyser!\n\n"
+    text_report += f"Here is your report on {text_title.title()}:\n\n"
+
+    # Basic stats
     text_report += cell_border
-    text_report += "|      Num. Words      |      Mean Word length    |\n"
+    text_report += "|{:^25}|{:^25}|\n".format("Num. Words", "Mean Word length")
     text_report += cell_border
     text_report += stats(num_words, words_mean_length)
+    text_report += cell_border + "\n"
+
     text_report += cell_border
-    text_report += "|    Num. Sentences    |     Mean Sent. Length    |\n"
+    text_report += "|{:^25}|{:^25}|\n".format("Num. Sentences", "Mean Sent. length")
     text_report += cell_border
     text_report += stats(num_sentences, sent_mean_len)
+    text_report += cell_border + "\n"
+
     text_report += cell_border
-    text_report += cell_border
-    text_report += "|    Num. Paragraphs   |    Mean Parag. Length    |\n"
+    text_report += "|{:^25}|{:^25}|\n".format("Num. Paragraphs", "Mean Para. length")
     text_report += cell_border
     text_report += stats(num_paragraphs, parags_mean_len)
     text_report += cell_border
+
+    # Word frequencies
     text_report += "\nMost Frequent Words:\n"
-    text_report += "\n {:<10}: {:<5}\n".format("Word", "Frequency")
+    text_report += "\n {:<15}: {:^8}\n".format("Word", "Frequency")
+    text_report += cell_border
+    for word in most_frequent_words_lst:
+        text_report += f"{word[0]:<15}:{word[1]:^8}\n"
     text_report += cell_border
     
-    for word in report_dict["word_frequencies"][:10]:
-        text_report += f"'{word[0] + "'":<10}: {word[1]}\n"
-        
-    text_report += "\nLeast Frequent Words (>0.1% to keep stat meaningful):\n"
-    text_report += "\n {:<10}: {:<5}\n".format("Word", "Frequency")
-
-    counter = 0
-    for word in report_dict["word_frequencies"][-1::-1]:
-        if word[1] > len(report_dict["word_frequencies"]) / 1000 and counter < 10:
-            text_report += f"'{word[0] + "'":<10}: {word[1]:<5}\n"
-            counter += 1
+    text_report += "\nLeast Frequent Words (>0.2% to keep stat meaningful):\n"
+    text_report += "\n {:<15}: {:^8}\n".format("Word", "Frequency")
+    text_report += cell_border
+    for word in least_frequent_words_lst:
+        text_report += f"{word[0]:<15}:{word[1]:^8}\n"
+    text_report += cell_border
     
     return text_report
-
 
 if __name__ == "__main__":
     main()
